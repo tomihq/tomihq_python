2b39e0d6057f2871e1897ec28dbcdc8bd3d78e6d
Tomás Hernández <hernandeztomas584@gmail.com>
2022-08-14 15:26:44 -0300

    ADD matrixs and loops

---
+++ python-loop-complexity.py
@@ -0,0 +1,29 @@
+import time
+
+#Time counter
+start_time = time.time();
+
+#Math Matrixs.
+column = ["A", "B", "C", "D"];
+rowValues = [1, 2, 3, 4];
+
+
+#Setting the colums name
+
+for col in column:
+    print (col, end = " ");
+print ("")
+
+#The first on iterate is the outer loop (col) and the second one is the inner loop (row)
+
+for idx, col in enumerate(column):
+    for jdx, row in enumerate(rowValues):
+        print(str(row+idx+jdx), end = " "); 
+    print ("")
+
+#See how much time take to loop the matrix
+print((time.time()-start_time))
+
+#With the timer, we can know how much time take the matrix to loops,
+#this help us to optimize our code.
+
